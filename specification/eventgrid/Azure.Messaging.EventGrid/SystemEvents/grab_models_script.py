176dd812c575df9e7a566e2b22d3583b01f504b4
l0lawrence <llawrence@microsoft.com>
2023-09-14 09:22:40 -0700

    alias added models

--- specification/eventgrid/Azure.Messaging.EventGrid/SystemEvents/grab_models_script.py
+++ specification/eventgrid/Azure.Messaging.EventGrid/SystemEvents/grab_models_script.py
@@ -30,6 +30,28 @@ file_names = [
     "Web"
 ]
 
+special_case =  {"ApiManagement": "ApiManagement",
+    "AppConfiguration": "AppConfiguration",
+    "Cache": "RedisCache",
+    "Communication": "AzureCommunicationServices",
+    "ContainerRegistry": "ContainerRegistry",
+    "ContainerService": "ContainerService",
+    "DataBox": "DataBox",
+    "Devices": "IotHub",
+    "EventGrid": "SystemEvents",
+    "EventHub": "EventHub",
+    "HealthcareApis": "HealthcareApis",
+    "KeyVault": "KeyVault",
+    "MachineLearningServices": "MachineLearningServices",
+    "Maps": "Maps",
+    "Media": "MediaServices",
+    "PolicyInsights": "PolicyInsights",
+    "Resources": "Resources",
+    "ServiceBus": "ServiceBus",
+    "SignalRService": "SignalRService",
+    "Storage": "Storage",
+    "Web": "Web"}
+
 model_names = []
 for file_name in file_names:
     name =f"{PATH}/azure-rest-api-specs/specification/eventgrid/Azure.Messaging.EventGrid/SystemEvents/"+file_name+".tsp"
@@ -41,6 +63,26 @@ for file_name in file_names:
                 model_names.append("EventGrid." + name)
 
 
+model_names_in_swagger = []
+for file_name, json_name in special_case.items():
+    name =f"{PATH}/azure-rest-api-specs/specification/eventgrid/data-plane/Microsoft.{file_name}/stable/2018-01-01/{json_name}.json"
+    with open(name) as f_in:
+        output= json.load(f_in)
+        for line in output["definitions"].keys():
+            model_names_in_swagger.append("EventGrid." + line)
+
+
+# Compare that we have all the same models
+a = set(model_names)
+b = set(model_names_in_swagger)
+if a == b:
+    pass
+else:
+    print("Not the same models")
+    print("Models in tsp but not in swagger")
+    print(a.difference(b))
+    print("Models in swagger but not in tsp")
+    print(b.difference(a))
 
 # For each model_name add @@include to it
 include_statement = "@@usage("
